d5b9fc2d709bde51f80f207dbe257d2b18b1efd9
Danilo Ibáñez <danilobepo@gmail.com>
2020-07-20 22:18:16 -0400

    Change password admin

--- functions/routes/adminAuth.js
+++ functions/routes/adminAuth.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express.Router();
 
 const { db, firebase } = require("../utils/init");
+const { AuthAdmin } = require("../utils/middlewareAuth");
 const { isEmpty } = require("../utils/functions");
 
-app.post("/signupAdmin", (req, res) => {
+app.post("/signup", (req, res) => {
   const credentials = {
     rut: req.body.rut.toString(),
     password: req.body.password.toString(),
@@ -52,7 +53,7 @@ app.post("/signupAdmin", (req, res) => {
     });
 });
 
-app.post("/loginAdmin", (req, res) => {
+app.post("/login", (req, res) => {
   const credentials = {
     rut: req.body.rut.toString(),
     password: req.body.password.toString(),
@@ -83,6 +84,47 @@ app.post("/loginAdmin", (req, res) => {
         token,
         rut: doc.data().rut,
       });
+    })
+    .catch((error) => {
+      switch (error.code) {
+        case "auth/user-not-found":
+          return res.status(500).json({ error: "User not found" });
+        case "auth/wrong-password":
+          return res.status(500).json({ error: "Wrong password" });
+        case "auth/invalid-email":
+          return res.status(500).json({ error: "Invalid user" });
+        default:
+          return res.status(500).json({ error: error.code });
+      }
+    });
+});
+
+app.post("/changePassword", AuthAdmin, (req, res) => {
+  const credentials = {
+    rut: req.body.rut.toString(),
+    password: req.body.password.toString(),
+    newPassword: req.body.newPassword.toString(),
+  };
+
+  let errors = {};
+  if (isEmpty(credentials.rut)) errors.rut = "Must no be empty";
+  if (isEmpty(credentials.password)) errors.password = "Must no be empty";
+  if (isEmpty(credentials.newPassword)) errors.newPassword = "Must no be empty";
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(
+      `${credentials.rut}@soynutriadmin.tk`,
+      credentials.password
+    )
+    .then((data) => {
+      return data.user.getIdToken();
+    })
+    .then((token) => {
+      firebase.auth().currentUser.updatePassword(credentials.newPassword);
+      return res.status(200).json({ message: "Password change", token });
     })
     .catch((error) => {
       switch (error.code) {
