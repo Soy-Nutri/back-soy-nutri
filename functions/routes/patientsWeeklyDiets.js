25b5958b277ea9a16c9e2e915ca540be690575a5
RodrigoAlejandroBP <mefistoalejandro@hotmail.com>
2020-07-24 21:18:05 -0400

    Avance de patientsweeklydiet

--- functions/routes/patientsWeeklyDiets.js
+++ functions/routes/patientsWeeklyDiets.js
@@ -6,33 +6,27 @@ const { isEmpty } = require("../utils/functions");
 const { AuthAdmin, AuthUser } = require("../utils/middlewareAuth");
 
 
-app.post("/addWeeklyDiet", AuthAdmin, (req, res) => {
+app.post("/addWeeklyDiet",(req, res) => {
     const rut = req.body.rut;
-    //Son 7 dias, dejarlo como coleccion de datos
-    //puede quedar vacio (hay que cambiarlo esto en el front)
-    const new_weekly_diet = {
-      date: req.body.date,
-      day:req.body.date,
-      breakfast: {
-        meal: req.body.breakfast,
-      },
-      lunch: {
-        time: req.body.lunch_time,
-        meal: req.body.lunch,
-      },
-      snack: {
-        meal: req.body.snack,
-      },
-      post_training: req.body.post_training,
-      dinner: {
-        meal: req.body.dinner,
-      }
-
-
+    var date = req.body.date;
+    const day = req.body.day;
+    const new_day = {    
+        breakfast:  req.body.breakfast,
+        lunch: req.body.lunch, 
+        snack: req.body.snack,
+        post_training: req.body.post_training,
+        dinner: req.body.dinner,
+       
     };
-  
-    let errors = {};
+
     const empty_error = "Must not be empty";
+
+    let errors = {};
+    if (isEmpty(rut)) errors.rut = empty_error;
+    if (isEmpty(date)) errors.date = empty_error;
+    else date = new Date(date).toISOString();
+    if (isEmpty(day)) errors.day= empty_error;
+
 
 
     if (Object.keys(errors).length > 0) return res.status(400).json(errors);
@@ -41,25 +35,67 @@ app.post("/addWeeklyDiet", AuthAdmin, (req, res) => {
       .get()
       .then((doc) => {
         if (doc.exists) {
-          if (doc.data().weekly_diets) {
+          
+          if (doc.data().weekly_diet) {
+
             let dates = [];
-            for (let i = 0; i < doc.data().weekly_diets.length; i++) {
-              dates.push(doc.data().weekly_diets[i].date);
+            for (let i = 0; i < doc.data().weekly_diet.length; i++) {
+              dates.push(doc.data().weekly_diet[i].date);
             }
-            if (dates.indexOf(new_weekly_diet.date) === -1) {
-              let weekly_diets = doc.data().weekly_diets;
-              weekly_diets.push(new_weekly_diet);
-              db.collection("patients").doc(rut).update({ weekly_diets });
-              return res.status(200).json({ message: "Daily diet added." });
+
+            if (dates.indexOf(date) !== -1) {
+              let weekly_diet = doc.data().weekly_diet;
+              
+
+              //Hacer comprobacion de que el dia que se este ingresando
+              //no exista ya en la semana del weekly diet
+
+
+              if(day=="martes" ){
+                let tuesday=new_day ;
+                weekly_diet.push(tuesday);
+              }
+              else if(day=="miercoles"){
+                let wednesday=new_day ;
+                weekly_diet.push(wednesday);
+
+              }
+              else if(day=="jueves"){
+                let thursday=new_day ;
+                weekly_diet.push(thursday);
+
+              }
+              else if(day=="viernes"){
+                let friday=new_day ;
+                weekly_diet.push(friday);
+
+              }
+              else if(day=="sabado"){
+                let saturday=new_day ;
+                weekly_diet.push(saturday);
+
+              }
+              else if(day=="domingo"){
+                let sunday=new_day ;
+                weekly_diet.push(sunday);
+              }
+              
+              db.collection("patients").doc(rut).update({weekly_diet});
+              return res.status(200).json({ message: "Weekly diet and a day has been added." });
             } else {
+              //ya que no existe la fecha ingresada agregar que abajo
               return res
                 .status(400)
-                .json({ error: "The date of this daily diet is already taken." });
+                .json({ error: "The date of this Weekly diet is already taken." });
             }
-          } else {
+          }else {
+            let monday = new_day;
+           // return res.status(405).json({date:date,[day]:new_day});
             db.collection("patients")
               .doc(rut)
-              .update({ weekly_diets: [new_weekly_diet] });
+              .update({ weekly_diet: [ {date:date,[day]:new_day}] });
+      
+            
             return res.status(200).json({ message: "Daily diet added." });
           }
         } else {
@@ -68,8 +104,5 @@ app.post("/addWeeklyDiet", AuthAdmin, (req, res) => {
       })
       .catch((err) => res.status(500).json({ err }));
   });
-
-
-
-  //modificar con el put //
+ 
 module.exports = app;
