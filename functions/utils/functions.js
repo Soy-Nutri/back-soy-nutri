c18fed7602e124759c34037d623b91a0df4eaeb9
Danilo Ibáñez <danilobepo@gmail.com>
2020-08-07 18:14:34 -0400

    Add get weekly diets

--- functions/utils/functions.js
+++ functions/utils/functions.js
@@ -3,4 +3,23 @@ const isEmpty = (string) => {
   else return false;
 };
 
-module.exports = { isEmpty };
+var normalize = (function () {
+  var from = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç",
+    to = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc",
+    mapping = {};
+
+  for (var i = 0, j = from.length; i < j; i++)
+    mapping[from.charAt(i)] = to.charAt(i);
+
+  return function (str) {
+    var ret = [];
+    for (var i = 0, j = str.length; i < j; i++) {
+      var c = str.charAt(i);
+      if (mapping.hasOwnProperty(str.charAt(i))) ret.push(mapping[c]);
+      else ret.push(c);
+    }
+    return ret.join("");
+  };
+})();
+
+module.exports = { isEmpty, normalize };
