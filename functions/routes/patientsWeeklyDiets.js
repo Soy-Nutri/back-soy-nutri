a20529ad19f13d05ba574b5ea9c85a77ff3e2071
Danilo Ibáñez <danilobepo@gmail.com>
2020-08-09 18:18:08 -0400

    Update and delete of weekly diets.

--- functions/routes/patientsWeeklyDiets.js
+++ functions/routes/patientsWeeklyDiets.js
@@ -3,7 +3,7 @@ const app = express.Router();
 
 const { db, firebase } = require("../utils/init");
 const { isEmpty, normalize } = require("../utils/functions");
-const { AuthAdmin, AuthUser } = require("../utils/middlewareAuth");
+const { AuthAdmin, decideMiddleware } = require("../utils/middlewareAuth");
 
 function removeItemFromArr(arr, item) {
   return arr.filter(function (e) {
@@ -16,7 +16,7 @@ las semanas comienzan los lunes.
 se agregan en español. 
 */
 
-app.post("/addWeeklyDiet", (req, res) => {
+app.post("/addWeeklyDiet", AuthAdmin, (req, res) => {
   const rut = req.body.rut;
   var date = req.body.date;
   const day = normalize(req.body.day.toLowerCase());
@@ -175,7 +175,7 @@ app.post("/addWeeklyDiet", (req, res) => {
     .catch((err) => res.status(500).json({ err }));
 });
 
-app.get("/getWeeklyDiets/:rut", (req, res) => {
+app.get("/getWeeklyDiets/:rut/:user", decideMiddleware, (req, res) => {
   const rut = req.params.rut;
   db.doc(`/patients/${rut}`)
     .get()
@@ -196,7 +196,7 @@ app.get("/getWeeklyDiets/:rut", (req, res) => {
     });
 });
 
-app.get("/getWeeklyDiets/:rut/:date", (req, res) => {
+app.get("/getWeeklyDiets/:rut/:date/:user", decideMiddleware, (req, res) => {
   const rut = req.params.rut;
   const date = req.params.date;
   db.doc(`/patients/${rut}`)
@@ -223,6 +223,219 @@ app.get("/getWeeklyDiets/:rut/:date", (req, res) => {
       } else {
         return res.status(400).json({ message: "Weekly diets not found." });
       }
+    });
+});
+
+app.put("/modifyWeeklyDiets", AuthAdmin, (req, res) => {
+  const rut = req.body.rut;
+  var date = req.body.date;
+  const day = normalize(req.body.day.toLowerCase());
+  const day_info = {
+    breakfast: req.body.breakfast,
+    lunch: req.body.lunch,
+    snack: req.body.snack,
+    post_training: req.body.post_training,
+    dinner: req.body.dinner,
+  };
+
+  const days_of_week = [
+    "lunes",
+    "martes",
+    "miercoles",
+    "jueves",
+    "viernes",
+    "sabado",
+    "domingo",
+  ];
+  const empty_error = "Must not be empty";
+  let errors = {};
+  if (isEmpty(rut)) errors.rut = empty_error;
+  if (isEmpty(date)) errors.date = empty_error;
+  else date = new Date(date).toISOString();
+  if (isEmpty(day)) {
+    errors.day = empty_error;
+  }
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  if (days_of_week.indexOf(day) === -1) {
+    return res.status(408).json({ message: "Day does not exist." });
+  }
+
+  db.doc(`/patients/${rut}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        if (doc.data().weekly_diets) {
+          let id = -1;
+          for (let i = 0; i < doc.data().weekly_diets.length; i++) {
+            if (doc.data().weekly_diets[i].date === date) {
+              id = i;
+            }
+          }
+          if (id !== -1) {
+            let new_day = doc.data().weekly_diets;
+            if (day in new_day[id]) {
+              if (day_info.breakfast !== "") {
+                new_day[id][day].breakfast = day_info.breakfast;
+              }
+              if (day_info.lunch !== "") {
+                new_day[id][day].lunch = day_info.lunch;
+              }
+              if (day_info.snack !== "") {
+                new_day[id][day].snack = day_info.snack;
+              }
+              if (day_info.post_training !== "") {
+                new_day[id][day].post_training = day_info.post_training;
+              }
+              if (day_info.dinner !== "") {
+                new_day[id][day].dinner = day_info.dinner;
+              }
+              db.doc(`/patients/${rut}`).update({ weekly_diets: new_day });
+              return res.status(200).json({ message: "Change data." });
+            } else {
+              return res.status(404).json({ message: "Day not found." });
+            }
+          } else {
+            return res.status(404).json({ message: "Date not found." });
+          }
+        } else {
+          return res.status(404).json({ message: "Weekly diets not found." });
+        }
+      } else {
+        return res.status(404).json({ message: "Patients not found." });
+      }
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err });
+    });
+});
+
+app.delete("/deleteWeeklyDiets", AuthAdmin, (req, res) => {
+  const rut = req.body.rut;
+  var date = req.body.date;
+  const day = normalize(req.body.day.toLowerCase());
+  const days_of_week = [
+    "lunes",
+    "martes",
+    "miercoles",
+    "jueves",
+    "viernes",
+    "sabado",
+    "domingo",
+  ];
+  const empty_error = "Must not be empty";
+  let errors = {};
+  if (isEmpty(rut)) errors.rut = empty_error;
+  if (isEmpty(date)) errors.date = empty_error;
+  else date = new Date(date).toISOString();
+  if (isEmpty(day)) {
+    errors.day = empty_error;
+  }
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  if (days_of_week.indexOf(day) === -1) {
+    return res.status(408).json({ message: "Day does not exist." });
+  }
+
+  db.doc(`/patients/${rut}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        if (doc.data().weekly_diets) {
+          let id = -1;
+          for (let i = 0; i < doc.data().weekly_diets.length; i++) {
+            if (doc.data().weekly_diets[i].date === date) {
+              id = i;
+            }
+          }
+          if (id !== -1) {
+            let new_day = doc.data().weekly_diets;
+            if (day in new_day[id]) {
+              delete new_day[id][day];
+              db.doc(`/patients/${rut}`).update({ weekly_diets: new_day });
+              return res.status(200).json({ message: "Day delete." });
+            } else {
+              return res.status(404).json({ message: "Day not found." });
+            }
+          } else {
+            return res.status(404).json({ message: "Date not found." });
+          }
+        } else {
+          return res.status(404).json({ message: "Weekly diets not found." });
+        }
+      } else {
+        return res.status(404).json({ message: "Patients not found." });
+      }
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err });
+    });
+});
+
+app.delete("/deleteWeekOfWeeklyDiets", AuthAdmin, (req, res) => {
+  const rut = req.body.rut;
+  var date = req.body.date;
+
+  const empty_error = "Must not be empty";
+  let errors = {};
+  if (isEmpty(rut)) errors.rut = empty_error;
+  if (isEmpty(date)) errors.date = empty_error;
+  else date = new Date(date).toISOString();
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  db.doc(`/patients/${rut}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        if (doc.data().weekly_diets) {
+          let new_weekly = [];
+          for (let i = 0; i < doc.data().weekly_diets.length; i++) {
+            if (doc.data().weekly_diets[i].date !== date) {
+              new_weekly.push(doc.data().weekly_diets[i]);
+            }
+          }
+          if (new_weekly.length !== doc.data().weekly_diets.length) {
+            db.doc(`/patients/${rut}`).update({ weekly_diets: new_weekly });
+            return res.status(200).json({ message: "Delete Week." });
+          } else {
+            return res.status(404).json({ message: "Weekly diets not found." });
+          }
+        } else {
+          return res.status(404).json({ message: "Weekly diets not found." });
+        }
+      } else {
+        return res.status(404).json({ message: "Patients not found." });
+      }
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err });
+    });
+});
+
+app.delete("/deleteAllWeeklyDiets", AuthAdmin, (req, res) => {
+  const rut = req.body.rut;
+
+  const empty_error = "Must not be empty";
+  let errors = {};
+  if (isEmpty(rut)) errors.rut = empty_error;
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  db.doc(`/patients/${rut}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        if (doc.data().weekly_diets) {
+          db.doc(`/patients/${rut}`).update({ weekly_diets: [] });
+          return res.status(200).json({ message: "Delete Week." });
+        } else {
+          return res.status(404).json({ message: "Weekly diets not found." });
+        }
+      } else {
+        return res.status(404).json({ message: "Patients not found." });
+      }
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err });
     });
 });
 
